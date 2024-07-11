d233a210cc5863487c614bec20a468611b1fa451
Dominic Clifton <me@dominicclifton.name>
2024-07-11 15:32:20 +0200

    VariantBuilder: Move the ordered assert macros into a mod.

---
+++ src/util/assert/mod.rs
@@ -0,0 +1,30 @@
+#[macro_use]
+#[allow(unused_macros)]
+pub mod ordered {
+    #[macro_export]
+    macro_rules! assert_contains_inorder {
+    ($content: expr, [$($element:expr,)+]) => {
+        let _index = 0;
+        let _remainder = &$content;
+        $(
+            let element = $element;
+            let message = format!("unmatched ordered element\nindex: {}\nelement:\n{}\ncontent:\n{}\n", _index, element, _remainder);
+            let (_, _remainder) = _remainder.split_once(element).expect(message.as_str());
+            let _index = _index + 1;
+        )*
+    }
+}
+
+    #[macro_export]
+    macro_rules! assert_inorder {
+    ($content:expr, $expected:expr) => {
+        {
+            let remainder = $content;
+            let expected = $expected;
+            let (_, remainder) = remainder.split_once(expected).expect(format!("expected string not found, expected: '{}', remaining content: '{}'", expected, remainder).as_str());
+
+            remainder.to_string()
+        }
+    };
+}
+}
--- tests/variantbuilder.rs
+++ tests/variantbuilder.rs
@@ -1,30 +1,8 @@
 // Run tests as follows:
 // `cargo test --features="cli"`
 
-macro_rules! assert_contains_inorder {
-    ($content: expr, [$($element:expr,)+]) => {
-        let _index = 0;
-        let _remainder = &$content;
-        $(
-            let element = $element;
-            let message = format!("unmatched ordered element\nindex: {}\nelement:\n{}\ncontent:\n{}\n", _index, element, _remainder);
-            let (_, _remainder) = _remainder.split_once(element).expect(message.as_str());
-            let _index = _index + 1;
-        )*
-    }
-}
-
-macro_rules! assert_inorder {
-    ($content:expr, $expected:expr) => {
-        {
-            let remainder = $content;
-            let expected = $expected;
-            let (_, remainder) = remainder.split_once(expected).expect(format!("expected string not found, expected: '{}', remaining content: '{}'", expected, remainder).as_str());
-
-            remainder.to_string()
-        }
-    };
-}
+#[macro_use]
+extern crate makerpnp;
 
 #[cfg(feature="cli")]
 mod tests {
