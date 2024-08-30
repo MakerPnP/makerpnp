b11b2e17434cfb3414158724302ef96462d2d294
Dominic Clifton <me@dominicclifton.name>
2024-08-30 14:52:22 +0200

    Failing to make a language selector dropdown

--- crates/planner_gui/src/main.rs
+++ crates/planner_gui/src/main.rs
@@ -3,18 +3,81 @@
     windows_subsystem = "windows"
 )]
 
+use std::ops::Index;
+use std::rc::Rc;
+use std::sync::{Mutex, MutexGuard};
 use freya::prelude::*;
 use dioxus_logger::tracing::{debug, Level};
 use freya::dioxus_core::AttributeValue;
 use planner_app::ViewModel;
 use crate::app_core::CoreService;
+use dioxus_sdk::{
+    i18n::{
+        use_i18,
+        use_init_i18n,
+        Language,
+    },
+    translate,
+};
 
 use dioxus_router::prelude::{Outlet, Routable, Router, use_navigator};
+use indexmap::IndexMap;
 
 mod app_core;
 
+static LANGUAGE_SET: Mutex<Option<IndexMap<String, String>>> = Mutex::new(None);
+static SELECTED_LANGUAGE: Mutex<Option<String>> = Mutex::new(None);
+
+fn load_languages() {
+
+    let language_set: IndexMap<String, String> = IndexMap::from([
+        ("en-US".to_string(), "English (United-States)".to_string()),
+        ("es-ES".to_string(), "Español (España)".to_string()),
+    ]);
+    
+    let keys = language_set.keys();
+    let first_language = keys.into_iter().next().unwrap();
+    
+    change_language(first_language);
+    
+    let mut guard = LANGUAGE_SET.lock().unwrap();
+    (*guard).replace(language_set);
+    
+}
+
+fn change_language(language_code: &String) {
+
+    let mut i18 = use_i18();
+
+    let mut guard = SELECTED_LANGUAGE.lock().unwrap();
+    (*guard).replace(language_code.clone());
+
+    i18.set_language(language_code.parse().unwrap());
+}
+
+fn language_set() -> MutexGuard<'static, Option<IndexMap<String, String>>> {
+    
+    let guard = LANGUAGE_SET.lock().expect("not locked");
+    
+    guard
+}
+
+// FIXME avoid cloning, return some reference instead, but how!
+fn selected_language() -> String {
+
+    let guard = SELECTED_LANGUAGE.lock().expect("not locked");
+
+    guard.as_ref().unwrap().clone()
+}
+
 fn app() -> Element {
-    rsx!(Router::<Route> {})
+
+    rsx!(
+        rect {
+           font_family: "Arimo Nerd",
+            Router::<Route> {}
+        }
+    )
 }
 
 #[derive(Routable, Clone, PartialEq)]
@@ -80,22 +143,66 @@ fn AppSidebar() -> Element {
         debug!("save clicked");
         app_core.send(planner_app::Event::Save );
     };
+    
+    let current_language_thing = use_signal(|| {
+        // WTF goes here ???
 
+        let selected_language_binding = selected_language();
+        let selected_language = selected_language_binding;
+
+        selected_language
+    });
+
+    
+    let language_set_thing = use_hook(|| {
+        let language_set_binding = language_set();
+        let language_set = language_set_binding.as_ref().unwrap();
+
+        // FIXME avoid cloning
+        language_set.clone()
+    });
 
     rsx!(
         NativeRouter {
+            rect {
+                width: "100%",
+                height: "32px",
+                direction: "horizontal",
+                background: "#3C3F41",
+
+                Button {
+                    onclick: on_click_create,
+                    label {
+                        "\u{ea7b} Create"
+                    }
+                },
+                Button {
+                    onclick: on_click_save,
+                    label {
+                        "\u{e27c} Save"
+                    }
+                },
+                // TODO some divider
+                Dropdown {
+                    value: current_language_thing.read().clone(),
+                    for (value, name) in language_set_thing {
+                        DropdownItem {
+                            value: value.clone(),
+                            onclick: {
+                                to_owned![value];
+                                move |_| change_language(&value)
+                            },
+                            label { "{name}" }
+                        }
+                    }
+                }
+            },
+
             Sidebar {
                 sidebar: rsx!(
                     SidebarItem {
-                        onclick: on_click_create,
                         label {
-                            "Create"
-                        }
-                    },
-                    SidebarItem {
-                        onclick: on_click_save,
-                        label {
-                            "Save"
+                            "TODO"
                         }
                     },
                 ),
@@ -112,10 +219,23 @@ fn AppSidebar() -> Element {
         }
     )
 }
-                    
+
+static ARIMO_NERD_FONT: &[u8] = include_bytes!("../assets/fonts/ArimoNerdFont-Regular.ttf");
+
+
+static EN_US: &str = include_str!("../assets/i18n/en-US.json");
+static ES_ES: &str = include_str!("../assets/i18n/es-ES.json");
+
 fn main() {
     dioxus_logger::init(Level::DEBUG).expect("failed to init logger");
     console_error_panic_hook::set_once();
 
-    launch(app); // Be aware that this will block the thread
+    load_languages();
+    
+    launch_cfg(
+        app,
+        LaunchConfig::<()>::builder()
+            .with_font("Arimo Nerd", ARIMO_NERD_FONT)
+            .build(),
+    );
 }
